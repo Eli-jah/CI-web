2112a3634955be353022d631ac842519adeb5385
elijah <1198331919@qq.com>
2019-01-15 13:48:30 +0800

    admin header logo added

--- shm/shmadmin/views/inc_header.php
+++ shm/shmadmin/views/inc_header.php
@@ -85,7 +85,9 @@
             <div class="container">
                 <ul class="nav-logo pull-left">
                     <li>
-                        <a href="http://www.sahereman.com/" target="_blank"></a>
+                        <a href="http://www.sahereman.com/" target="_blank">
+                            <img style="height:24px;" src="/web/shmweb/assets/images/color_logo.png" alt="Sahereman.com">
+                        </a>
                     </li>
                     <li>
                         <a href="<?php echo site_url() ?>"> <?php echo $this->config->item('title_suffix'); ?> </a></li>
